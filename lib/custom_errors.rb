aa8b45f3521999380d2e8d0ad542d5228ed5adf2
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-30 22:17:18 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,21 +1,18 @@
 class Person
-  attr_accessor :partner, :name
-
+  attr_accessor :name, :partner
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
     person.partner = self
   end
-
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
 
 
